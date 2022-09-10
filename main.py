bf42133f70739a19f805db7b45aacf72c5da7f60
LeoAlbu125 <64243684+LeoAlbu125@users.noreply.github.com>
2022-09-10 20:25:41 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -50,33 +50,40 @@ def main():
     link = r"https://www.pichau.com.br/hardware/placa-de-video?page=1"
 
     driver = webdriver.Edge(EdgeChromiumDriverManager().install())
-    driver.get(link)
+    driver.get(link) #Opens the automated browser
     NunPages = driver.find_element(by=By.XPATH,value=r'//*[@id="__next"]/main/div[2]/div/div[1]/nav/ul/li[8]/button').text
+    #gets the maximun number of pages
     try:
-        for pages in range(1,int(NunPages)+1):
-            driver.get("https://www.pichau.com.br/hardware/placa-de-video?page={}".format(str(pages)))
-            list_path = driver.find_elements(by=By.XPATH,value = r'//*[@id="__next"]/main/div[2]/div/div[1]/div[2]/div/a/div/div[2]/h2')
-            list_driver_name = [na.text for na in list_path]
-            f_name_list.extend(list_driver_name)
+        for pages in range(1,int(NunPages)+1): #Looping to read all pages
             
+            driver.get("https://www.pichau.com.br/hardware/placa-de-video?page={}".format(str(pages)))
+            
+            #-----------get GPU names------------------------------------------------------------------------------------------------------#
+            list_path = driver.find_elements(by=By.XPATH,value = r'//*[@id="__next"]/main/div[2]/div/div[1]/div[2]/div/a/div/div[2]/h2') #generate a list with all xpaths
+            list_driver_name = [na.text for na in list_path] #extract the text of each xpath
+            f_name_list.extend(list_driver_name) #add to the list
+            #------------------------------------------------------------------------------------------------------------------------------#
+            
+            #----------get GPU prices--------------------------------------------------------------------------------------------------------------------------------#
             list_path = driver.find_elements(by=By.XPATH,value = r"//main/div/div/div/div/div/a/div/div/div/div[1]/div/div")
             list_driver_price = [nb.text.replace("R$","").replace(".","").replace(",",".").replace(" ","") for nb in list_path if "de" not in nb.text]
+            #filter texts in the GPU value
 
             for letters in string.ascii_letters+":":
-                list_driver_price = [xil.replace(letters,"") for xil in list_driver_price]
-            f_price_discount_list.extend(list_driver_price)
+                list_driver_price = [xil.replace(letters,"") for xil in list_driver_price] #filter every latter and : from the prices value
+            f_price_discount_list.extend(list_driver_price) #add to the list
             
             list_path = driver.find_elements(by=By.XPATH,value =r"//main/div/div/div/div/div/a/div/div/div/div[3]/div/div")
             list_driver_price_full = [float(nc.text.replace("R$ ","").replace(".","").replace(",",".")) for nc in list_path if "sem juros no cartão" not in nc.text]
             f_price_full_list.extend(list_driver_price_full)
-            
-            print(len(f_name_list),len(f_price_discount_list),len(f_price_full_list))
-            if len(f_name_list) != len(f_price_discount_list):
+            #--------------------------------------------------------------------------------------------------------------------------------------------------------#
+            print(len(f_name_list),len(f_price_discount_list),len(f_price_full_list)) #print the size of each list (name, discounted price and price)
+            if len(f_name_list) != len(f_price_discount_list): #if a list has diferent size stops the program
                 break
             
-        dtset = pd.DataFrame({"name":f_name_list[:len(f_price_discount_list)],"Price Discounted":f_price_discount_list,"Price Full":f_price_full_list})
-        dtset.to_csv("GPU_price.csv",index=False)
-        driver.quit()
+        dtset = pd.DataFrame({"name":f_name_list[:len(f_price_discount_list)],"Price Discounted":f_price_discount_list,"Price Full":f_price_full_list}) #unify all lists in a dataframe
+        dtset.to_csv("GPU_price.csv",index=False)# save dataframe in csv
+        driver.quit() #close the driver browser
     except Exception as E:
         print(E)
 
